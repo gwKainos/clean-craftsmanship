dcce973d71851a66bc07375aa85916db4d880a2e
Kainos <gwkainos@gmail.com>
2024-12-30 23:54:17 +0900

    Test: 테스트 코드 한줄 작성

--- src/test/java/kr/kainos/clean/basic/StackTest.java
+++ src/test/java/kr/kainos/clean/basic/StackTest.java
@@ -1,10 +1,13 @@
 package kr.kainos.clean.basic;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 
 public class StackTest {
   @Test
   public void canCreateStack() throws Exception {
     Stack stack = new Stack();
+    assertFalse(stack.isEmpty());
   }
 }
