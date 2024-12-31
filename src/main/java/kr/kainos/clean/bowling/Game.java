fc74c2c792eabf03a77ccc46225a8acbe67e3da4
Kainos <gwkainos@gmail.com>
2024-12-31 15:44:07 +0900

    Refactor: 리팩토링 - 설계 변경을 했지만 실패

--- src/main/java/kr/kainos/clean/bowling/Game.java
+++ src/main/java/kr/kainos/clean/bowling/Game.java
@@ -1,12 +1,21 @@
 package kr.kainos.clean.bowling;
 
 public class Game {
-  private int score;
+
+  private int rolls[] = new int[21];
+  private int currentRoll = 0;
+
   public void roll(int pins) {
-    score += pins;
+    rolls[currentRoll++] = pins;
   }
 
   public int score() {
+    int score = 0;
+    for (int i = 0; i < rolls.length; i++) {
+      if (rolls[i] + rolls[i + 1] == 10 && i % 2 == 0) {
+        score += rolls[i];
+      }
+    }
     return score;
   }
 }
