c38a3f2a6743a077a2efac50ff38803405098a8e
Kainos <gwkainos@gmail.com>
2024-12-31 12:35:05 +0900

    Test: 테스트가 성공할 수 있도록 수정

--- src/test/java/kr/kainos/clean/factorization/PrimeFactorsTest.java
+++ src/test/java/kr/kainos/clean/factorization/PrimeFactorsTest.java
@@ -1,19 +1,20 @@
 package kr.kainos.clean.factorization;
 
-import static java.util.Optional.empty;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyCollectionOf;
 import static org.hamcrest.Matchers.is;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class PrimeFactorsTest {
   @Test
   public void factors() throws Exception {
-    assertThat(factorsOf(1), is(empty()));
+    assertThat(factorsOf(1), is(emptyCollectionOf(Integer.class)));
   }
 
   private List<Integer> factorsOf(int n) {
-    return null;
+    return new ArrayList<>();
   }
 }
