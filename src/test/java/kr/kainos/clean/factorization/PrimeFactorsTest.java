7e8d45cf26f4b5649f38ba5f2c1d81a3a22747b5
Kainos <gwkainos@gmail.com>
2024-12-31 12:31:12 +0900

    Feat: 기능 구현 - Null 리턴 - 오류 발생

--- src/test/java/kr/kainos/clean/factorization/PrimeFactorsTest.java
+++ src/test/java/kr/kainos/clean/factorization/PrimeFactorsTest.java
@@ -4,11 +4,16 @@ import static java.util.Optional.empty;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class PrimeFactorsTest {
   @Test
   public void factors() throws Exception {
     assertThat(factorsOf(1), is(empty()));
+  }
+
+  private List<Integer> factorsOf(int n) {
+    return null;
   }
 }
