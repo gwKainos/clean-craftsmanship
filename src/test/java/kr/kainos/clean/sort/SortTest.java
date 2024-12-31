7da6cc40a70e95deea447a4d8e15320a66c663db
Kainos <gwkainos@gmail.com>
2024-12-31 19:01:01 +0900

    Test: 실패하는 테스트 코드 작성

--- src/test/java/kr/kainos/clean/sort/SortTest.java
+++ src/test/java/kr/kainos/clean/sort/SortTest.java
@@ -3,7 +3,6 @@ package kr.kainos.clean.sort;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +11,8 @@ public class SortTest {
   public void sorted() throws Exception {
     assertEquals(asList(), sort(asList()));
     assertEquals(asList(1), sort(asList(1)));
+    assertEquals(asList(1, 2), sort(asList(1, 2)));
+    assertEquals(asList(1, 2), sort(asList(2, 1)));
   }
 
   private List<Integer> sort(List<Integer> list) {
