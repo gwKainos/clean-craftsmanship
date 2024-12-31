e63261d967d7024b8ed1cbdc19da7e2a7052b4da
Kainos <gwkainos@gmail.com>
2024-12-31 13:52:15 +0900

    Test: 테스트 코드 추가

--- src/test/java/kr/kainos/clean/bowling/BowlingTest.java
+++ src/test/java/kr/kainos/clean/bowling/BowlingTest.java
@@ -9,4 +9,10 @@ public class BowlingTest {
   public void canCreateGame() throws Exception {
     Game g = new Game();
   }
+
+  @Test
+  public void canRoll() throws Exception {
+    Game g = new Game();
+    g.roll(0);
+  }
 }
