bdfe5e7275d88e1eea34d0b5dab3d3de24d6aba9
Kainos <gwkainos@gmail.com>
2024-12-31 18:25:01 +0900

    Feat: 코드 수정 - 조건절 추가

--- src/main/java/kr/kainos/clean/bowling/Game.java
+++ src/main/java/kr/kainos/clean/bowling/Game.java
@@ -14,12 +14,16 @@ public class Game {
     int frameIndex = 0;
 
     for (int frame = 0; frame < 10; frame++) {
-      if (isSpare(frameIndex) == 10) {
+      if (rolls[frameIndex] == 10) {
+        score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        frameIndex += 1;
+      } else if (isSpare(frameIndex) == 10) {
         score += 10 + rolls[frameIndex + 2];
+        frameIndex += 2;
       } else {
         score += rolls[frameIndex] + rolls[frameIndex + 1];
+        frameIndex += 2;
       }
-      frameIndex += 2;
     }
     return score;
   }
