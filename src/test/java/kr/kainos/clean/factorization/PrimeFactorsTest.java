1d7d2b3adb8bb0c6698d313e09b81fd66251c2a4
Kainos <gwkainos@gmail.com>
2024-12-31 12:38:26 +0900

    Test: 테스트 코드 한줄 작성 - 실패하는 가장 간단하고, 가장 구체적이며, 가장 퇴화한 테스트 작성

--- src/test/java/kr/kainos/clean/factorization/PrimeFactorsTest.java
+++ src/test/java/kr/kainos/clean/factorization/PrimeFactorsTest.java
@@ -3,6 +3,7 @@ package kr.kainos.clean.factorization;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.emptyCollectionOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.contains;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ public class PrimeFactorsTest {
   @Test
   public void factors() throws Exception {
     assertThat(factorsOf(1), is(emptyCollectionOf(Integer.class)));
+    assertThat(factorsOf(2), is(contains(2)));
   }
 
   private List<Integer> factorsOf(int n) {
