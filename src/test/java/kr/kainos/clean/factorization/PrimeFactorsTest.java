39e3ef9647480af6ebb987c33432a3ea4ce7aba2
Kainos <gwkainos@gmail.com>
2024-12-31 13:05:34 +0900

    Feat: 3으로 나누는 반복문 추가

--- src/test/java/kr/kainos/clean/factorization/PrimeFactorsTest.java
+++ src/test/java/kr/kainos/clean/factorization/PrimeFactorsTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class PrimeFactorsTest {
+
   @Test
   public void factors() throws Exception {
     assertThat(factorsOf(1), is(emptyCollectionOf(Integer.class)));
@@ -25,15 +26,22 @@ public class PrimeFactorsTest {
 
   private List<Integer> factorsOf(int n) {
     ArrayList<Integer> factors = new ArrayList<>();
-    if(n > 1) {
+    if (n > 1) {
       while (n % 2 == 0) {
         factors.add(2);
         n /= 2;
       }
+
+      while (n % 3 == 0) {
+        factors.add(3);
+        n /= 3;
+      }
     }
-    if(n > 1) {
+
+    if (n > 1) {
       factors.add(n);
     }
+
     return factors;
   }
 }
