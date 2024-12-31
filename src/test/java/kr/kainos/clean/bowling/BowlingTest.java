f93fdc8b1110c8d2244b6020ff355cfbfafd95da
Kainos <gwkainos@gmail.com>
2024-12-31 15:07:56 +0900

    Test: 테스트 코드 수정

--- src/test/java/kr/kainos/clean/bowling/BowlingTest.java
+++ src/test/java/kr/kainos/clean/bowling/BowlingTest.java
@@ -1,21 +1,21 @@
 package kr.kainos.clean.bowling;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BowlingTest {
 
   private Game g;
 
-  @Test
+  @BeforeEach
   public void setUp() throws Exception {
     g = new Game();
   }
 
   @Test
   public void gutterGame() throws Exception {
-    for(int i=0; i<20; i++) {
+    for(int i = 0; i < 20; i++) {
       g.roll(0);
     }
     assertEquals(0, g.score());
