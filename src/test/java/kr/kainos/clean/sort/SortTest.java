189d116d49bf27a006ed039f5aea434e588d5363
Kainos <gwkainos@gmail.com>
2025-01-01 20:46:10 +0900

    Feat: 여러개의 원소를 처리하기 위해 반복문 추가

--- src/test/java/kr/kainos/clean/sort/SortTest.java
+++ src/test/java/kr/kainos/clean/sort/SortTest.java
@@ -10,19 +10,26 @@ public class SortTest {
 
   @Test
   public void sorted() throws Exception {
-    assertEquals(asList(), sort(asList()));
-    assertEquals(asList(1), sort(asList(1)));
+    assertEquals(List.of(), sort(List.of()));
+    assertEquals(List.of(1), sort(List.of(1)));
     assertEquals(asList(1, 2), sort(asList(1, 2)));
     assertEquals(asList(1, 2), sort(asList(2, 1)));
+    assertEquals(asList(1, 2, 3), sort(asList(1, 2, 3)));
+    assertEquals(asList(1, 2, 3), sort(asList(2, 1, 3)));
   }
 
   private List<Integer> sort(List<Integer> list) {
     if (list.size() > 1) {
-      if (list.get(0) > list.get(1)) {
-        int first = list.get(0);
-        int second = list.get(1);
-        list.set(0, second);
-        list.set(1, first);
+
+      for (int firstIndex = 0; firstIndex < list.size() - 1; firstIndex++) {
+        int secondIndex = firstIndex + 1;
+
+        if (list.get(firstIndex) > list.get(secondIndex)) {
+          int first = list.get(firstIndex);
+          int second = list.get(secondIndex);
+          list.set(firstIndex, second);
+          list.set(secondIndex, first);
+        }
       }
     }
     return list;
