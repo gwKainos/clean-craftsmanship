43894398c9edc0713cfa9ad23bf76061a77cabb2
Kainos <gwkainos@gmail.com>
2025-01-01 20:55:06 +0900

    Feat: 성공하는 코드로 수정

--- src/test/java/kr/kainos/clean/sort/SortTest.java
+++ src/test/java/kr/kainos/clean/sort/SortTest.java
@@ -17,19 +17,23 @@ public class SortTest {
     assertEquals(asList(1, 2, 3), sort(asList(1, 2, 3)));
     assertEquals(asList(1, 2, 3), sort(asList(2, 1, 3)));
     assertEquals(asList(1, 2, 3), sort(asList(3, 2, 1)));
+    assertEquals(asList(1, 1, 2, 3, 3, 3, 4, 5, 5, 5, 6, 7, 8, 9, 9, 9),
+            sort(asList(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3)));
   }
 
   private List<Integer> sort(List<Integer> list) {
     if (list.size() > 1) {
 
-      for (int firstIndex = 0; firstIndex < list.size() - 1; firstIndex++) {
-        int secondIndex = firstIndex + 1;
+      for (int limit = list.size() -1; limit > 0; limit--) {
+        for (int firstIndex = 0; firstIndex < limit; firstIndex++) {
+          int secondIndex = firstIndex + 1;
 
-        if (list.get(firstIndex) > list.get(secondIndex)) {
-          int first = list.get(firstIndex);
-          int second = list.get(secondIndex);
-          list.set(firstIndex, second);
-          list.set(secondIndex, first);
+          if (list.get(firstIndex) > list.get(secondIndex)) {
+            int first = list.get(firstIndex);
+            int second = list.get(secondIndex);
+            list.set(firstIndex, second);
+            list.set(secondIndex, first);
+          }
         }
       }
     }
