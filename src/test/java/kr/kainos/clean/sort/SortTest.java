ca04e761fb200cb59dceaf503464139a0f47f286
Kainos <gwkainos@gmail.com>
2025-01-01 21:30:00 +0900

    Test: 실패하는 테스트 코드 작성

--- src/test/java/kr/kainos/clean/sort/SortTest.java
+++ src/test/java/kr/kainos/clean/sort/SortTest.java
@@ -13,6 +13,7 @@ public class SortTest {
     assertEquals(List.of(), sort(List.of()));
     assertEquals(List.of(1), sort(List.of(1)));
     assertEquals(asList(1, 2), sort(asList(1, 2)));
+    assertEquals(asList(1, 2), sort(asList(2, 1)));
   }
 
   private List<Integer> sort(List<Integer> list) {
