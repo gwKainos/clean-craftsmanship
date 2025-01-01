9b5980b29bc656a9a01412ccc0a4b4a52e45c1f6
Kainos <gwkainos@gmail.com>
2025-01-01 20:33:11 +0900

    Feat: 성공하는 코드로 수정

--- src/test/java/kr/kainos/clean/sort/SortTest.java
+++ src/test/java/kr/kainos/clean/sort/SortTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class SortTest {
+
   @Test
   public void sorted() throws Exception {
     assertEquals(asList(), sort(asList()));
@@ -16,6 +17,14 @@ public class SortTest {
   }
 
   private List<Integer> sort(List<Integer> list) {
+    if (list.size() > 1) {
+      if (list.get(0) > list.get(1)) {
+        int first = list.get(0);
+        int second = list.get(1);
+        list.set(0, second);
+        list.set(1, first);
+      }
+    }
     return list;
   }
 }
