40ca21b8d044c281ff0002388e774d4e9a60b85d
Kainos <gwkainos@gmail.com>
2025-01-02 19:35:53 +0900

    Feat: 테스트 코드에 의한 필요 코드 구현

---
+++ src/test/java/kr/kainos/clean/AuthenticatorSpy.java
@@ -0,0 +1,19 @@
+package kr.kainos.clean;
+
+public class AuthenticatorSpy {
+
+  public void setResult(boolean b) {
+  }
+
+  public int getCount() {
+    return 1;
+  }
+
+  public String getLastUsername() {
+    return "user";
+  }
+
+  public String getLastPassword() {
+    return "pw";
+  }
+}
