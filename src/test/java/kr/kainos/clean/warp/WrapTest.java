a8204e008b13a348e17a632c236f24e181bde858
Kainos <gwkainos@gmail.com>
2025-01-02 12:24:52 +0900

    Test: 실패하는 테스트 코드 작성

--- src/test/java/kr/kainos/clean/warp/WrapTest.java
+++ src/test/java/kr/kainos/clean/warp/WrapTest.java
@@ -13,6 +13,9 @@ public class WrapTest {
     assertWrapped("xx", 1, "x\nx");
     assertWrapped("xx", 2, "xx");
     assertWrapped("xxx", 1, "x\nx\nx");
+    assertWrapped("xxx", 2, "xx\nx");
+    assertWrapped("xxx", 3, "xxx");
+    assertWrapped("x x", 1, "x\nx");
   }
 
   private void assertWrapped(String s, int width, String expected) {
