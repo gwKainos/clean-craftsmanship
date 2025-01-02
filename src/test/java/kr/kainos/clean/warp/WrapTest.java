ee3fa0ee0b32b76f4cd2bd7dab0665ee70c883d4
Kainos <gwkainos@gmail.com>
2025-01-02 12:39:15 +0900

    Test: 실패하는 테스트 코드 작성

--- src/test/java/kr/kainos/clean/warp/WrapTest.java
+++ src/test/java/kr/kainos/clean/warp/WrapTest.java
@@ -18,6 +18,16 @@ public class WrapTest {
     assertWrapped("xxx", 3, "xxx");
     assertWrapped("x x", 1, "x\nx");
     assertWrapped("x x", 2, "x\nx");
+    assertWrapped("x x", 3, "x x");
+    assertWrapped("x x x", 1, "x\nx\nx");
+    assertWrapped("x x x", 2, "x\nx\nx");
+    assertWrapped("x x x", 3, "x x\nx");
+    assertWrapped("x x x", 4, "x x\nx");
+    assertWrapped("x x x", 5, "x x x");
+    assertWrapped("xx xx", 1, "x\nx\nx\nx");
+    assertWrapped("xx xx", 2, "xx\nxx");
+    assertWrapped("xx xx", 3, "xx\nxx");
+    assertWrapped("xx xx", 4, "xx\nxx");
   }
 
   private void assertWrapped(String s, int width, String expected) {
@@ -29,6 +39,6 @@ public class WrapTest {
       return s;
     }
 
-    return s.substring(0, w) + "\n" + wrap(s.substring(w).trim(), w);
+    return s.substring(0, w).trim() + "\n" + wrap(s.substring(w).trim(), w);
   }
 }
