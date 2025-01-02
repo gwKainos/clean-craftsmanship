4d4fc0625900d64aa1691256814a0b10df3b3284
Kainos <gwkainos@gmail.com>
2025-01-02 19:47:50 +0900

    Feat: 스파이 구현

--- src/test/java/kr/kainos/clean/AuthenticatorSpy.java
+++ src/test/java/kr/kainos/clean/AuthenticatorSpy.java
@@ -1,19 +1,34 @@
 package kr.kainos.clean;
 
-public class AuthenticatorSpy {
+import kr.kainos.clean.application.service.Authenticator;
 
-  public void setResult(boolean b) {
+public class AuthenticatorSpy implements Authenticator {
+
+  private int callCount = 0;
+  private boolean result = false;
+  private String lastUsername = null;
+  private String lastPassword = null;
+
+  public Boolean authenticate(String username, String password) {
+    callCount++;
+    lastUsername = username;
+    lastPassword = password;
+    return result;  // 미리 설정된 결과 반환
+  }
+
+  public void setResult(boolean result) {
+    this.result = result;
   }
 
   public int getCount() {
-    return 1;
+    return callCount;
   }
 
   public String getLastUsername() {
-    return "user";
+    return lastUsername;
   }
 
   public String getLastPassword() {
-    return "pw";
+    return lastPassword;
   }
 }
