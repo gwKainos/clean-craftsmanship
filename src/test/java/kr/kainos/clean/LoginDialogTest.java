66b860e0d129a8ff72d8bb4b6e13f43d5cb73376
Kainos <gwkainos@gmail.com>
2025-01-02 18:33:27 +0900

    Test: 테스트 코드 작성

--- src/test/java/kr/kainos/clean/LoginDialogTest.java
+++ src/test/java/kr/kainos/clean/LoginDialogTest.java
@@ -1,7 +1,7 @@
 package kr.kainos.clean;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import kr.kainos.clean.application.service.Authenticator;
 import kr.kainos.clean.application.ui.Event;
@@ -9,6 +9,11 @@ import kr.kainos.clean.application.ui.LoginDialog;
 import org.junit.jupiter.api.Test;
 
 public class LoginDialogTest {
+  private static Authenticator getAuthenticator() {
+    Authenticator authenticator = new AuthenticatorDummy();
+    return authenticator;
+  }
+
   @Test
   public void whenClosed_loginIsCancelled() throws Exception {
     Authenticator authenticator = getAuthenticator();
@@ -19,8 +24,12 @@ public class LoginDialogTest {
     assertTrue(success);
   }
 
-  private static Authenticator getAuthenticator() {
-    Authenticator authenticator = new AuthenticatorDummy();
-    return authenticator;
+  @Test
+  public void whenAuthenticatorRejects_loginFails() throws Exception {
+    Authenticator authenticator = new RejectingAuthenticator;
+    LoginDialog dialog = new LoginDialog(authenticator);
+    dialog.show();
+    boolean success = dialog.sendEvent(Event.CLOSE);
+    assertFalse(success);
   }
 }
