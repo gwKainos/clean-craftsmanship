a44a274816737fd817609427ef63224b51c512e2
Kainos <gwkainos@gmail.com>
2025-01-03 12:55:30 +0900

    Refactor: 코드 리팩토링 - 영화 목록 기반으로 예외 처리.

--- src/main/java/kr/kainos/clean/video/Customer.java
+++ src/main/java/kr/kainos/clean/video/Customer.java
@@ -1,16 +1,42 @@
 package kr.kainos.clean.video;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Customer {
 
+  private String title;
   private int days;
+  private Map<String, VideoType> movieRegistry = new HashMap<>();
+
+  enum VideoType {
+    REGULAR,
+    CHILDRENS
+  }
+
+  ;
+
+  public Customer() {
+    movieRegistry.put("RegularMovie", VideoType.REGULAR);
+    movieRegistry.put("ChildrensMovie", VideoType.CHILDRENS);
+  }
 
   public void addRental(String title, int days) {
+    this.title = title;
     this.days = days;
   }
 
   public int getRentalFee() {
-    return applyGracePeriod(150, 3);
+    if (getType(title) == VideoType.REGULAR) {
+      return applyGracePeriod(150, 3);
+    }
+    return 100;
   }
+
+  private VideoType getType(String title) {
+    return movieRegistry.get(title);
+  }
+
 
   public int getRenterPoints() {
     return applyGracePeriod(1, 3);
