54b9201987febf91029c17d92da2e7e275ebc647
Kainos <gwkainos@gmail.com>
2025-01-03 11:43:30 +0900

    Feat: 테스트 코드에 의한 필요 코드 구현 - 필드 초기화 자동 생성 : Option + Enter - 변수로 만들기 : Command + Option + V

--- src/main/java/kr/kainos/clean/video/Customer.java
+++ src/main/java/kr/kainos/clean/video/Customer.java
@@ -2,14 +2,25 @@ package kr.kainos.clean.video;
 
 public class Customer {
 
+  private int days;
+
   public void addRental(String title, int days) {
+    this.days = days;
   }
 
   public double getRentalFee() {
-    return 1.5;
+    double fee = 1.5;
+    if (days > 3) {
+      fee += 1.5 * (days - 3);
+    }
+    return fee;
   }
 
   public int getRenterPoints() {
-    return 1;
+    int points = 1;
+    if (days > 3) {
+      points += (days -3);
+    }
+    return points;
   }
 }
